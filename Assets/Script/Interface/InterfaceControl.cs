90f57f223ca574040102b4fdcd559c3c7f5b05af
Coreficent <coreficent@gmail.com>
2021-03-16 22:59:57 -0400

    Implement interface control.

---
+++ Assets/Script/Interface/InterfaceControl.cs
@@ -0,0 +1,26 @@
+﻿namespace Coreficent.Interface
+{
+    using Coreficent.Main;
+    using Coreficent.Utility;
+    using UnityEngine;
+
+
+    public class InterfaceControl : MonoBehaviour
+    {
+
+        public void Render()
+        {
+            DebugUtility.Bug("render called");
+        }
+
+        public void SelectPreset(int index)
+        {
+
+        }
+
+        public void Slide()
+        {
+
+        }
+    }
+}
